365e1aee71c6cde5d5c134ffd824baa35eff85a1
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2024-03-21 17:16:53 +0800

    Added GameData menu

---
+++ GameDataMenu.cs
@@ -0,0 +1,32 @@
+﻿using KitchenData;
+using UnityEngine;
+
+namespace KitchenECSExplorer
+{
+    internal class GameDataMenu : PlateUpExplorerMenu
+    {
+        private Vector2 scrollPosition = new Vector2(0, 0);
+
+        ObjectData Data;
+
+        private const float windowWidth = 775f;
+
+        public GameDataMenu()
+        {
+            ButtonName = "GameData";
+        }
+
+        protected override void OnInitialise()
+        {
+            Data = new ObjectData("GameData", GameData.Main);
+        }
+
+        protected override void OnSetup() // This is called evey frame the menu is open, This is also where you draw your UnityGUI
+        {
+            GUILayout.BeginArea(new Rect(10f, 0f, windowWidth, 1000f));
+            GUI.DrawTexture(new Rect(0, 0, windowWidth, 1000f), Background);
+            DrawObjectHierarchy(Data, ref scrollPosition);
+            GUILayout.EndArea();
+        }
+    }
+}
